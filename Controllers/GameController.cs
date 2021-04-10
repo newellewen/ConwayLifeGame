d7247962649d63dc1f33e28bde273c6e8e45e201
Phil Newell <newell.philip.86@gmail.com>
2021-04-09 20:10:12 -0400

    Cleaning things up, handling growth betterer

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -25,11 +25,15 @@ namespace ConwayLifeGame.Controllers
 
         public void Go()
         {
+            //TODO - something better with the max+2 in FillList, handle positive/negative coordinates or apply transform
+            FillList();
+
+            //TODO - something better than setting the neighbors every single time
+            SetNeighbours();
+
             CheckRulesTask();
-            //CheckRules.Wait();
 
             ApplyRulesTask();
-            //ApplyRules.Wait();
         }
 
         private void FillList()
@@ -38,9 +42,9 @@ namespace ConwayLifeGame.Controllers
             int x_max = cells.Max(cell => cell.x);
             int y_max = cells.Max(cell => cell.y);
 
-            for (int i = 0; i < x_max; i++)
+            for (int i = 0; i < x_max + 2; i++)
             {
-                for (int j = 0; j < y_max; j++)
+                for (int j = 0; j < y_max + 2; j++)
                 {
                     Cell cell = new Cell(i, j);
                     if (cells.Where(_cell => _cell.Equals(cell)).Count() == 0)
@@ -62,13 +66,13 @@ namespace ConwayLifeGame.Controllers
 
         public void PrintState()
         {
-            foreach(Cell cell in cells)
+            foreach(Cell cell in cells.Where(_cell => _cell.IsAlive()))
             {
                 Console.WriteLine(cell);
-                foreach(Cell neighbour in cell.neighbours)
-                {
-                    Console.WriteLine("      " + neighbour);
-                }
+                //foreach(Cell neighbour in cell.neighbours)
+                //{
+                //    Console.WriteLine("      " + neighbour);
+                //}
             }
         }
 
@@ -80,7 +84,7 @@ namespace ConwayLifeGame.Controllers
 
             foreach (Cell cell in cells)
             {
-                int liveNeighbours = cell.neighbours.Where(cell => cell.IsAlive()).Count();
+                int liveNeighbours = cell.neighbours.Where(_cell => _cell.IsAlive()).Count();
                 if (cell.IsAlive())
                 {
                     if (liveNeighbours < 2 || liveNeighbours > 3)
@@ -113,49 +117,4 @@ namespace ConwayLifeGame.Controllers
 
     }
 
-
-    //    private Task CheckRulesTask()
-    //    {
-    //        return Task.Factory.StartNew(delegate ()
-    //        {
-    //            cellsToKill = new List<Cell>();
-    //            cellsToResurrect = new List<Cell>();
-
-    //            foreach (Cell cell in cells)
-    //            {
-    //                int liveNeighbours = cell.neighbours.Where(cell => cell.IsAlive()).Count();
-    //                if (cell.IsAlive())
-    //                {
-    //                    if (liveNeighbours < 2 || liveNeighbours > 3)
-    //                    {
-    //                        cellsToKill.Add(cell);
-    //                    }
-    //                } else
-    //                {
-    //                    if (liveNeighbours == 3)
-    //                    {
-    //                        cellsToResurrect.Add(cell);
-    //                    }
-    //                }
-    //            }
-    //        });
-    //    }
-
-    //    private Task ApplyRulesTask()
-    //    {
-    //        return Task.Factory.StartNew(delegate ()
-    //        {
-    //            foreach (Cell cell in cellsToKill)
-    //            {
-    //                cells.Find(_cell => cell.Equals(cell)).Kill();
-    //            }
-
-    //            foreach (Cell cell in cellsToResurrect)
-    //            {
-    //                cells.Find(_cell => cell.Equals(cell)).Resurrect();
-    //            }
-    //        });
-    //    }
-
-    //}
 }
--- Program.cs
+++ Program.cs
@@ -13,17 +13,21 @@ namespace ConwayLifeGame
         {
             List<Cell> cells = new List<Cell>();
 
-            cells.Add(new Cell(2, 3, true));
-            cells.Add(new Cell(2, 4, true));
-            cells.Add(new Cell(2, 5, true));
-            cells.Add(new Cell(2, 6, true));
-            cells.Add(new Cell(2, 7, true));
+            cells.Add(new Cell(3, 2, true));
             cells.Add(new Cell(3, 3, true));
             cells.Add(new Cell(3, 4, true));
             cells.Add(new Cell(3, 5, true));
 
             GameController game = new GameController(cells);
 
+
+            //TODO - Iterations!
+            game.PrintState();
+            game.Go();
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------------------");
+            game.PrintState();
+            game.Go();
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------------------");
             game.PrintState();
             game.Go();
             Console.WriteLine("---------------------------------------------------------------------------------------------------------------------");
